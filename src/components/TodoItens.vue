6ec5d136b68f9b44cff4db7f808d30301ad3ba0a
Victor Iki <victooriki@gmail.comm>
2024-04-24 17:40:54 -0300

    componentes and router

---
+++ src/components/TodoItens.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="space-y-2">
+        <TodoItem />
+        <TodoItem />
+    </div>
+</template>
+
+<script>
+    import TodoItem from './TodoItem.vue'
+    
+    export default {
+        components: {
+            TodoItem
+        }
+    }
+</script>
---
+++ src/router/index.js
@@ -0,0 +1,17 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: () => import('../App.vue')
+  },
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+})
+
+export default router
+
